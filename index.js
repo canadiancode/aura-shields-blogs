ce0136b85aa72575cfbd853548ad48d27e6e0904
Kevin Heidema <heidemakevin@gmail.com>
2025-05-23 23:55:25 -0700

    Overview of the project

---
+++ index.js
@@ -0,0 +1,15 @@
+// query Shopify for the list of blog categories
+
+// get every single blog post
+
+// feed the list of categories and the curent blog posts to openAI with a pre-prompt
+
+// get ai to return a json object with following keys & values:
+    // - title
+    // - body (in HTML format)
+    // - meta title
+    // - meta description
+
+// extract values, add to object to add to Shopify blogs
+
+// add to Shopify blogs
